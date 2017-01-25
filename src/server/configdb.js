8b0c8ebf6658b09479f2dc28465aa53c8764f5fd
daurgil <daurgil@gmail.com>
2017-01-25 20:31:39 +0100

    Added .gitignore files to MySQL

--- src/server/configdb.js
+++
@@ -1,9 +0,0 @@
-console.log('mysql');
-var mysql = require('mysql');
-exports.connection = mysql.createConnection({
-        host: '127.0.0.1',
-        user: 'daurgil',
-        password: '',
-        database: 'rot'
-    }
-);
