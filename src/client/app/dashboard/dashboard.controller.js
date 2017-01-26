32925eab7600ea73fbd9a7416eee0be6b6079705
Antonio Revert Hinojosa <a.revert.hinojosa@gmail.com>
2017-01-26 13:20:42 +0100

    Finished dashboard with background working, pending to change their name now

--- src/client/app/dashboard/dashboard.controller.js
+++ src/client/app/dashboard/dashboard.controller.js
@@ -9,25 +9,24 @@
   /* @ngInject */
   function DashboardController($q, dataservice, logger) {
     var vm = this;
-    // vm.news = {
-    //   title: 'Repair on time',
-    //   description: 'Hot Towel Angular is a SPA template for Angular developers.'
-    // };
+    vm.news = {
+      title: 'Repair on time',
+      description: 'Hot Towel Angular is a SPA template for Angular developers.'
+    };
 
-    vm.title = 'Dashboard';
+    vm.title = 'Main';
 
-    // vm.myVideo = {
-    // mp4: 'https://repairontime.tk/view/video/mp4/back.mp4'/*,
-    // ogg: 'http://repairontime.tk/view/video/ogv/back.ogv',
-    // webm: 'http://repairontime.tk/view/video/webm/back.webm'*/
-    // };
+    vm.myVideo = {
+    mp4: './video/back.mp4',
+    ogg: './video/back.ogv',
+    webm: './video/back.webm'
+    };
 
     // vm.config = {
 		// 		sources: [
-		// 			// {src: $sce.trustAsResourceUrl('https://repairontime.tk/view/video/mp4/back.mp4'), type: 'video/mp4'},
-    //       {src: $sce.trustAsResourceUrl('//static.videogular.com/assets/videos/videogular.mp4'), type: 'video/mp4'}/*,
-		// 			{src: $sce.trustAsResourceUrl('https://repairontime.tk/view/video/webm/back.webm'), type: 'video/webm'},
-		// 			{src: $sce.trustAsResourceUrl('https://repairontime.tk/view/video/ogv/back.ogv'), type: 'video/ogg'}*/
+		// 			{src: $sce.trustAsResourceUrl('./video/back.mp4'), type: 'video/mp4'},
+		// 			{src: $sce.trustAsResourceUrl('./video/back.webm'), type: 'video/webm'},
+		// 			{src: $sce.trustAsResourceUrl('./video/back.ogv'), type: 'video/ogg'}
 		// 		],
     //     plugins: {
     //       poster: 'https://repairontime.tk/view/img/static.jpg'
