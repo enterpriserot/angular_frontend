ac7d3947e271057eec2dcd400ef72e633b5b2717
daurgil <daurgil@gmail.com>
2017-01-29 13:23:18 +0100

    Push simple changes for technicians module

--- src/client/app/technicians/technicians.controller.js
+++ src/client/app/technicians/technicians.controller.js
@@ -5,13 +5,21 @@
         .module('app.technicians')
         .controller('techniciansController', techniciansController);
 
-    techniciansController.$inject = ['$q','dataservice','logger'];
+    techniciansController.$inject = ['$q','dataservice','logger','$scope'];
 
     /* @ngInject */
-    function techniciansController($q, dataservice, logger) {
+    function techniciansController($q, dataservice, logger, $scope) {
         var vm = this;
         vm.title = 'Technicians';
         vm.technicians= [];
+        vm.filteredTechnicians = [];
+        vm.numPerPage = 5;
+        vm.maxSize = 5;
+        vm.currentPage = 1;
+
+        $scope.$watch(update);
+        // var watcher = vm.watch('./technicians.html');
+        // watcher.on('change', update);
 
         vm.map = { center: { latitude: 38.8101561, longitude: -0.6043774 }, zoom: 10 };
 
@@ -30,6 +38,12 @@
             vm.technicians = data;
             return vm.technicians;
           });
+        }
+
+        function update(){
+            var begin = ((vm.currentPage - 1) * vm.numPerPage), end = begin + vm.numPerPage;
+            vm.filteredTechnicians = vm.technicians.slice(begin, end);
+            console.log(vm.filteredTechnicians);
         }
     }
 })();
