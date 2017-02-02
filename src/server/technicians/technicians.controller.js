0e8208b434c546822702cbdfeb910be8b310f6c6
daurgil <daurgil@gmail.com>
2017-02-02 20:45:29 +0100

    Technicians MAP shows markers, with geolocation and delimited radio area

--- src/server/technicians/technicians.controller.js
+++ src/server/technicians/technicians.controller.js
@@ -1,7 +1,7 @@
 var technicians = require('./technicians.model');
 
 exports.getTechnicians = function(req, res){
-  technicians.getTechnicians(
+  technicians.getTechnicians(req.body,
     function (err, technicians) {
       if(err){
         res.send(err);
--- src/server/technicians/technicians.model.js
+++ src/server/technicians/technicians.model.js
@@ -1,15 +1,32 @@
 
 var mysql = require('../config.db');
+// var utils = require('../utils/functions.js');
 
 
 var techniciansModel= {};
 
-techniciansModel.getTechnicians = function (callback){
+techniciansModel.getTechnicians = function (location, callback){
   if (mysql.connection) {
-        mysql.connection.query('SELECT * FROM technicians ORDER BY id', function(error, rows) {
+    console.log(location);
+    var distance = 0.09009; /*Equivale a 10 Km, 1 = 111 Km*/
+    var lat = location.latitude;
+    var minLat = lat-distance;
+    var maxLat = lat+distance;
+
+    var lng = location.longitude;
+    var minLng = lng-distance;
+    var maxLng = lng+distance;
+
+    // select * from technicians
+    // WHERE (latitude >= '38.7319693' AND latitude <= '38.9121493') AND (longitude >= '-0.6964827' AND longitude <= '-0.5163027') ORDER BY id
+    console.log('SELECT * FROM technicians WHERE ((latitude >= "'+minLat+'" AND latitude <= "'+maxLat+'") AND (longitude >= "'+minLng+'" AND longitude <= "'+maxLng+'")) ORDER BY id');
+    // var box = utils.getboundaries(lat, lng, distance);
+
+        mysql.connection.query('SELECT * FROM technicians WHERE ((latitude >= "'+minLat+'" AND latitude <= "'+maxLat+'") AND (longitude >= "'+minLng+'" AND longitude <= "'+maxLng+'")) ORDER BY id', function(error, rows) {
             if(error){
                 throw error;
             }else{
+              console.log(rows);
                 callback(null, rows);
             }
         });
