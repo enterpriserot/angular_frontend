5c5f00daeeb815e09e2fec5fa1290411062a1591
Antonio Revert Hinojosa <a.revert.hinojosa@gmail.com>
2017-02-15 23:39:26 +0100

    Removed video module, because cause a lot of problems with the test

--- src/client/app/main/main.module.js
+++ src/client/app/main/main.module.js
@@ -3,9 +3,6 @@
 
   angular.module('app.main', [
     'app.core',
-    'app.widgets'/*,
-    'com.2fdevs.videogular',
-    'com.2fdevs.videogular.plugins.overlayplay',
-    'com.2fdevs.videogular.plugins.poster'*/
+    'app.widgets'
   ]);
 })();
--- src/client/app/widgets/widgets.module.js
+++ src/client/app/widgets/widgets.module.js
@@ -1,6 +1,6 @@
 (function() {
   'use strict';
 
-  angular.module('app.widgets', ['video-background', 'uiGmapgoogle-maps']);
+  angular.module('app.widgets', ['uiGmapgoogle-maps']);
 
 })();
