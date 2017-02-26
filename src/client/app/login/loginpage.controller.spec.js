69b0f93e7b7406a18562522890e6a9733b654407
Antonio Revert Hinojosa <a.revert.hinojosa@gmail.com>
2017-02-26 12:51:15 +0100

    Added new test to loginpage controller spec

--- src/client/app/login/loginpage.controller.spec.js
+++ src/client/app/login/loginpage.controller.spec.js
@@ -4,7 +4,7 @@ describe('LoginPageController', function() {
 
   beforeEach(function() {
     bard.appModule('app.login');
-    bard.inject('$controller', '$log', '$rootScope', '$http');
+    bard.inject('$controller', '$log', '$rootScope', '$http', '$q', '$state');
   });
 
   beforeEach(function() {
@@ -31,12 +31,24 @@ describe('LoginPageController', function() {
       it('function loginSend should be defined', function() {
         expect(controller.loginSend).to.be.defined;
       });
+
+      it('function goToSignup should be defined', function() {
+        expect(controller.goToSignup).to.be.defined;
+      });
     });
 
     describe('loginSend functionality', function() {
       it('password incorrect is shown if is detected on server', function() {
         var res = { data : 'errorcredentials' };
-
+        ds = {
+          login : function(data) {
+            return $q.when(res);
+          }
+        };
+        var controller = $controller('LoginPageController', {dataservice : ds});
+        controller.loginSend();
+        $rootScope.$apply();
+        expect(controller.error).to.equal('e-mail / password incorrect');
       });
     });
   });
